b6a56c2ddf8c2d4a3b9ed8d38ee4d3d9966c0188
SwatiPuhan <swatiPuhan59@gmail.com>
2024-08-19 14:15:12 +0530

    commit

---
+++ NewProject_Jenkins/src/test/java/demo/SampleTest.java
@@ -0,0 +1,19 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	@Test
+	
+	public void jenkinPracticeTest()
+	{
+		System.out.println("executed");
+	}
+	
+	public void jenkinTest()
+	{
+		System.out.println("executed 1st one");
+	}
+	
+
+}
